a0a2033aa481d646ca63b665c55e3756fbd4ce75
Ifeanyi Orji <chrisifwax101@gmail.com>
2018-11-14 09:44:43 +0100

    Test script fixed

---
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/AsyncTaskTest.java
@@ -0,0 +1,27 @@
+package com.udacity.gradle.builditbigger;
+
+import android.test.AndroidTestCase;
+import android.util.Log;
+
+import java.util.concurrent.ExecutionException;
+
+public class AsyncTaskTest extends AndroidTestCase {
+    private String mResult;
+
+    public void testAsyncTask() {
+        BackendAsyncTask backendAsyncTask = new BackendAsyncTask(null);
+        backendAsyncTask.execute(getContext());
+
+        try {
+            mResult = backendAsyncTask.get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        Log.w("Test result", mResult);
+        assertNotNull(mResult);
+    }
+
+}
